ae2d296ab23e974da2f1a0ae17b5c830a077aa7a
AlexIvanGonzalez <alex41860gonz@gmail.com>
2024-10-05 22:34:31 +0000

    ejer.C

---
+++ PuntoC.sh
@@ -0,0 +1,38 @@
+   64  pwd
+   65  ll
+   66  tree Examenes-UTN/
+   67  sudo useradd developer
+   68  cat /etc/passwd
+   69  sudo groupadd grupoDevops
+   70  cat /etc/group
+   71  id developer
+   72  sudo usermod -aG grupoDevops developer
+   73  id developer
+   74  sudo useradd tester
+   75  sudo groupadd grupoDiseño
+   76  sudo usermod -aG grupoDiseño tester
+   77  id tester
+   78  sudo useradd devops
+   79  sudo groupadd grupoDeveloper
+   80  sudo usermod -aG grupoDeveloper devops
+   81  id devops
+   82  sudo chmod -R 750 /Examenes-UTN/alumno_1
+   83  ls Examenes-UTN/
+   84  ll
+   85  cd Examenes-UTN/
+   86  ll
+   87  sudo chmod -R 750 /Examenes-UTN/alumno_1
+   88  sudo chmod -R 750 /alumno_1
+   89  cd ..
+   90  sudo chmod -R 750 /Examenes-UTN/alumno_1
+   91  ls Examenes-UTN/
+   92  sudo chmod -R 750 Examenes-UTN/alumno_1
+   93  sudo chmod -R 760 Examenes-UTN/alumno_2
+   94  sudo chmod -R 700 Examenes-UTN/alumno_3
+   95  sudo chmod -R 775 Examenes-UTN/profesores
+   96  whoami > /Examenes-UTN/alumno_1/validar.txt
+   97  whoami > Examenes-UTN/alumno_1/validar.txt
+   98  whoami > validar.txt
+   99  ll
+  100  history
+
