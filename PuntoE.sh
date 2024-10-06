a96660b936fbcbc016603c3f580790fc91b57e2c
AlexIvanGonzalez <alex41860gonz@gmail.com>
2024-10-06 02:48:29 +0000

    ejer. E y F

--- PuntoE.sh
+++ PuntoE.sh
@@ -1,1 +1,8 @@
+  117  cat /proc/meminfo
+  118  grep MemTotal /proc/meminfo
+  119  ll
+  120  grep MemTotal /proc/meminfo > filtroBasico.txt
+  121  cat filtroBasico.txt
+  122  sudo dmidecode -t chassis |grep Manufacturer >> filtroBasico.txt
+  123  cat filtroBasico.txt
 }
