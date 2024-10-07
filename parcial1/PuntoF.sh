4288ebbdae7e4edb4f60ef948eac882f717c2089
AlexIvanGonzalez <alex41860gonz@gmail.com>
2024-10-07 21:52:50 +0000

    corrección Punto.F

--- parcial1/PuntoF.sh
+++ parcial1/PuntoF.sh
@@ -1,6 +1,9 @@
 131  cat << EOF |tee $ARCHIVO_FILTRO_AVANZADO
 Mi IP Publica es: $(curl -s ifconfig.me)
-Mi usuario es: $(whoami)
+Mi usuario es: $(grep ":$(id -u):" /etc/passwd | awk -F ':' '{print $1}')
 El Hash de mi usuario es: $(sudo cat /etc/shadow |grep vagrant |awk -F ':' '{print $2}')
 EOF
+132 history
+133 cat > filtroAvanzado.txt
+134 vim filtroAvanzado.txt
 
